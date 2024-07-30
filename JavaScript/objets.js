cf7b6cd885ced61736567856bd29bac90b6f3ba7
MatiasGEscobar <matiie9135@gmail.com>
2024-07-30 19:26:13 -0300

    clase 8

---
+++ JavaScript/objets.js
@@ -0,0 +1,73 @@
+var deportes = {
+    conBalon: ["Football","Basquetball", "Voley"], 
+    sinBalon: ["Boxeo", "Surf", "Trekking"],
+};
+
+var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+
+// ACCEDER
+var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+console.log(persona.edad);
+
+// ASIGNAR
+var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+persona.nombre = 'Martín';
+console.log(persona.nombre);
+
+//CREAR
+var autos = {};
+autos.marcas = ['Ford', 'Audi', 'Ferrari'];
+console.log(autos);
+
+//BORRAR
+delete autos.marcas;
+console.log(autos);
+
+// DOT NOTATION
+var atuendos = { manos: ['Guantes', 'Anillos'], pies: ['Zapatos', 'Soquetes'] };
+
+// BRACKET NOTATION
+atuendos['piernas'] = ['Bermudas', 'Pantalones'];
+console.log(atuendos);
+
+// HAS OWN PROPERTY
+var libro = { autor: 'Borges', genero: 'Policial', año: 1990 };
+var tienePropiedad = libro.hasOwnProperty('nombre');
+
+console.log(tienePropiedad);
+
+// KEYS
+var libro = { autor: 'Borges', genero: 'Policial', año: 1990 };
+var todasLasPropiedades = Object.keys(libro);
+
+console.log(todasLasPropiedades);
+for (let prop in mundo) {
+   console.log('Esta es la propiedad: ', prop);
+   console.log('Este es el valor: ', mundo[prop]);
+}
+
+
+//THIS
+var mascota = {
+   animal: 'Perro',
+   raza: 'Ovejero Alemán',
+   amistoso: true,
+   dueño: 'María López',
+   info: () => {
+      console.log('Mi perro es un  ' + this.raza);
+   },
+};
+
+
+//CALLBACK
+function devuelvoUsuario() {
+   return 'CAMILO';
+}
+function devuelvoSaludo() {
+   return 'Hola';
+}
+function saludar(cb1, cb2) {
+   return cb1() + ' ' + cb2();
+}
+var resultado = saludar(devuelvoSaludo, devuelvoUsuario);
+console.log(resultado);
